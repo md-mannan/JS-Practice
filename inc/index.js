41ddcb35e3257757e3cfe15f12d3d4b084e3e172
md-mannan <mdmannan580@gmail.com>
2023-11-12 23:31:20 +0300

    array from method use to string to create array

--- inc/index.js
+++ inc/index.js
@@ -1,7 +1,7 @@
 // JS array method
 
-var numbers = [30, 34, 32, 50, 55, 60, 50, 67, 100]
-var result=numbers.filter(function(item){
-return item<50
-})
-document.write(result)
+// from method
+
+var title = 'The Quick brown fox jumps over the lazy dog.'
+var arryTitle = Array.from(title)
+document.write(arryTitle)
