952c41d801b1646b930fa3cb82cc9257122eb5df
md-mannan <mdmannan580@gmail.com>
2023-11-12 23:35:18 +0300

    Arrayfilter method use to filter   by condationaly

--- inc/index.js
+++ inc/index.js
@@ -1,7 +1,11 @@
 // JS array method
 
-// from method
+// filter method use to filter number by condationaly
 
-var title = 'The Quick brown fox jumps over the lazy dog.'
-var arryTitle = Array.from(title)
-document.write(arryTitle)
+var numbers = [10, 20, 30, 40, 50, 55, 60, 45, 60]
+
+var result = numbers.filter(function (item) {
+    return item > 50;
+})
+
+document.write(result)
