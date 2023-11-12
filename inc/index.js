46ad88c5cefb4d4fc9dec6aa8e2abb9ef7ff8f3d
md-mannan <mdmannan580@gmail.com>
2023-11-12 23:26:05 +0300

    array Filter

--- inc/index.js
+++ inc/index.js
@@ -1,16 +1,7 @@
- // JS Array
+// JS array method
 
-      var category = [
-        "Fasion",
-        "Beauty",
-        "Home  Aplience",
-        "Crocaries",
-        "Grocery",
-      ];
-      // var i;
-      // for (i = 0; i < category.length; i++) {
-      //   document.write(`${i}.` + category[i] + "<br/>");
-      // }
-      for (var item in category) {
-        document.write(`${item}.` + category[item] + "<br/>");
-      }
+var numbers = [30, 34, 32, 50, 55, 60, 50, 67, 100]
+var result=numbers.filter(function(item){
+return item<50
+})
+document.write(result)
